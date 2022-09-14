0d36b9cc57cd500ea5ed9c252f738968bb47360f
rlorxl <imjoeun08@naver.com>
2022-09-14 17:44:59 +0900

    section4 ~ [66]

--- useState/src/components/NewExpense/ExpenseForm.jsx
+++ useState/src/components/NewExpense/ExpenseForm.jsx
@@ -1,38 +1,54 @@
 import React, { useState } from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
-  const [title, setTitle] = useState('');
-  const [amount, setAmount] = useState('');
-  const [date, setDate] = useState('');
-
-  // const [userInput, setUserInput] = useState({
-  //   title: '',
-  //   amount: '',
-  //   date: '',
-  // });
+const ExpenseForm = props => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
 
   const titleChangeHandler = event => {
-    setTitle(event.target.value);
+    setEnteredTitle(event.target.value);
     // setUserInput({ ...userInput, title: event.target.value });
   };
 
   const amountChangeHandler = event => {
-    setAmount(event.target.value);
+    setEnteredAmount(event.target.value);
     // setUserInput({ ...userInput, amount: event.target.value });
   };
 
   const dateChangeHandler = event => {
-    setDate(event.target.value);
+    setEnteredDate(event.target.value);
     // setUserInput({ ...userInput, date: event.target.value });
   };
 
+  const submitHandler = e => {
+    e.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    props.onSaveExpenseData(expenseData); // 부모 컴포넌트와 소통하는 법: 부모 컴포넌트에서 정의된 함수를 호출하며 인자를 전달한다.
+
+    // reset input
+    setEnteredAmount('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          {/* 양방향 바인딩 : 입력에서 변경사항 수신 -> 상태 업데이트 -> 입력에 상태를 다시 보내줌. */}
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -40,6 +56,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -49,6 +66,7 @@ const ExpenseForm = () => {
             type="date"
             min="2022-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
--- useState/src/components/NewExpense/NewExpense.jsx
+++ useState/src/components/NewExpense/NewExpense.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = () => {
+const NewExpense = props => {
+  const saveExpenseDataHandler = enteredExpenseData => {
+    // 파라미터로 받은 값은 자식 컴포넌트인 ExpenseForm에서 끌어올려진 값임.
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      {/* onSaveExpenseData라는 이름의 props로 함수를 전달한다. 이때 전달하는 함수는 실행하지 않는다. 이 함수를 가리키기만 할 뿐이다. */}
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
