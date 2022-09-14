531a4a48a3ce1098e37267156ffbd953f3069f80
rlorxl <imjoeun08@naver.com>
2022-09-15 00:11:05 +0900

    section5 ~ [81] 조건부렌더링, 차트추가

--- useState/src/components/Expenses/Expenses.jsx
+++ useState/src/components/Expenses/Expenses.jsx
@@ -1,32 +1,34 @@
-import ExpenseItem from './ExpenseItem';
+import React, { useState } from 'react';
+
+import ExpensesList from './ExpensesList';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
 import Card from '../UI/Card';
 import './Expenses.css';
 
-function Expenses(props) {
-  return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.data[0].title}
-        amount={props.data[0].amount}
-        date={props.data[0].date}
-      />
-      <ExpenseItem
-        title={props.data[1].title}
-        amount={props.data[1].amount}
-        date={props.data[1].date}
-      />
-      <ExpenseItem
-        title={props.data[2].title}
-        amount={props.data[2].amount}
-        date={props.data[2].date}
-      />
-      <ExpenseItem
-        title={props.data[3].title}
-        amount={props.data[3].amount}
-        date={props.data[3].date}
-      />
-    </Card>
+const Expenses = props => {
+  const [filteredYear, setFilteredYear] = useState('2022');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter(
+    expense => expense.date.getFullYear().toString() === filteredYear
   );
-}
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
 
 export default Expenses;
--- useState/src/components/NewExpense/NewExpense.jsx
+++ useState/src/components/NewExpense/NewExpense.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = props => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = enteredExpenseData => {
     // 파라미터로 받은 값은 자식 컴포넌트인 ExpenseForm에서 끌어올려진 값임.
     const expenseData = {
@@ -10,12 +12,28 @@ const NewExpense = props => {
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
+      {!isEditing ? (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      ) : (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
       {/* onSaveExpenseData라는 이름의 props로 함수를 전달한다. 이때 전달하는 함수는 실행하지 않는다. 이 함수를 가리키기만 할 뿐이다. */}
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
